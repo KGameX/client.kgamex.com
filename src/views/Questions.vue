b93e614b123acf96f66e78e32e42822e1f11f46a
KGameX <kgamex.vk@gmail.com>
2026-05-08 16:35:22 +0200

    Some bugfixes

--- src/views/Questions.vue
+++ src/views/Questions.vue
@@ -11,24 +11,22 @@
                 <span class="button">{{ t('questions.ask') }}</span>
             </router-link>
         </section>
-
-        <div v-if="questionStore.questions.length > 0 && !questionStore.loading">        
-            <section class="question-list">
-                <router-link v-for="question in questionStore.questions" :key="question.id" :to="`/questions/${question.id}`" class="question-card">
-                    <div>
-                        <h6>{{ question.body }}</h6>
-                        <p :title="question.user?.username">{{ t('questions.asked', { username: question.user?.display_name || t('questions.anonymous'), date: new Date(question.created_at).toLocaleDateString() }) }}</p>
-                    </div>
-                    
-                    <div>
-                        <p>
-                            <span class="block" :class="{ 'answered': question.answer }">
-                                {{ question.answer ? t('questions.status.answered') : t('questions.status.unanswered') }}
-                            </span>
-                        </p>
-                    </div>
-                </router-link>
-            </section>
+     
+        <div class="question-list" v-if="questionStore.questions.length > 0 && !questionStore.loading">
+            <router-link v-for="question in questionStore.questions" :key="question.id" :to="`/questions/${question.id}`" class="question-card">
+                <div>
+                    <h6>{{ question.body }}</h6>
+                    <p :title="question.user?.username">{{ t('questions.asked', { username: question.user?.display_name || t('questions.anonymous'), date: new Date(question.created_at).toLocaleDateString() }) }}</p>
+                </div>
+                
+                <div>
+                    <p>
+                        <span class="block" :class="{ 'answered': question.answer }">
+                            {{ question.answer ? t('questions.status.answered') : t('questions.status.unanswered') }}
+                        </span>
+                    </p>
+                </div>
+            </router-link>
         </div>
         
         <section v-else-if="questionStore.loading">
